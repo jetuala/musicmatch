2a7123cc111e1ab593a5c80eb4086cbbb4a4ac31
Jonathan Tuala <jetuala@gmail.com>
2018-02-08 16:50:31 -0800

    Trying with audio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  // const rainbowArray = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+  const rainbowArray = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   //
   // var RandomRainbow = function() {
   //   return(
@@ -6,22 +6,33 @@
   //   );
   // }
 
+  var audio = new Audio('Piano_C4.aiff');
+
   var randColor = function() {
-      return '#' + Math.floor(Math.random()*16777215).toString(16);
+      return Math.floor(Math.random()*7);
   }
 
-  var Box = () => {
+  class Box extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+      color: rainbowArray[randColor()]
+      }
+    }
+
+    render() {
       return <div style={{
-          display: "inline-block",
-          background: randColor(),
-          width: "100px",
-          height: "100px",
-          border: "4px",
-          borderStyle: "solid",
-          borderColor: "white",
-          borderRadius: "3px",
-          margin: "0 2px"
-      }} />;
+        display: "inline-block",
+        background: this.state.color,
+        width: "100px",
+        height: "100px",
+        border: "4px",
+        borderStyle: "solid",
+        borderColor: "white",
+        borderRadius: "3px",
+        margin: "0 2px"
+      }} onClick={()=> audio.play('Piano_C4.aiff')}/>;
+    }
   }
 
   const Row = () => {
